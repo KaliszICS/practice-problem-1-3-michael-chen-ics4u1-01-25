0b608b34f03d01c30897911161d660d45ced0658
michael-chen-ics4u1-01-25 <michael.chen29@student.tdsb.on.ca>
2025-10-02 14:59:51 -0400

    completed set

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,59 @@
+import java.io.FileReader;
+import java.io.IOException;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	
+	public static String readFile(String filename) {
+		FileReader file = null;
+		String a = "";
+		try {
+			file = new FileReader(filename);
+			int b;
+			while ((b = file.read()) != -1) {
+				a += (char)b;
+			}
+		} catch (IOException e) {
+			return null;
+		} finally {
+			try {
+				if (file != null) {
+					file.close();
+				}
+			}
+			catch (IOException e) {
+				return "hi";
+			}
+		}
+		return a;
+	}
+
+	public static String backwardsReadFile(String filename) {
+		FileReader file = null;
+		String a = "";
+		try {
+			file = new FileReader(filename);
+			int b;
+			while ((b = file.read()) != -1) {
+				a += (char)b;
+			}
+		} catch (IOException e) {
+			return null;
+		} finally {
+			try {
+				if (file != null) {
+					file.close();
+				}
+			}
+			catch (IOException e) {
+				System.out.println(e);
+			}
+		}
+		return new StringBuilder(a).reverse().toString();
+	}
+
 
 }
